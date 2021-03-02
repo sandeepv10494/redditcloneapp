903ccf898ebabddbfd7710df5d79b3b7886a89c0
Sandeep V <sandeepv.10494@gmail.com>
2021-03-02 17:51:48 +0530

    Added swagger UI API documentation for APIs

--- src/main/java/com/sv/redditclone/RedditCloneApplication.java
+++ src/main/java/com/sv/redditclone/RedditCloneApplication.java
@@ -1,11 +1,14 @@
 package com.sv.redditclone;
 
+import com.sv.redditclone.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class RedditCloneApplication {
 
 	public static void main(String[] args) {
